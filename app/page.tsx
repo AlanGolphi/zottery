428e74fd655c11d3eafee8cc57ad041c110b46e2
AlanGolphi <alangolphi@163.com>
2024-08-18 15:58:09 +0800

    test: use wagmi to read and write from Lock contract

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import ConnectButton from '@/components/ConnectButton'
 import PopUpLocalWallet from '@/components/PopUpLocalWallet'
+import { TestReadContract } from '@/components/TestReadContract'
 
 export default function Home() {
   return (
     <>
       <ConnectButton />
       <PopUpLocalWallet />
+      <TestReadContract />
     </>
   )
 }
