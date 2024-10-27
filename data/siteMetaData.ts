daae9c23f9a7b6a20dcd755d1516955bc44f0edc
AlanGolphi <alangolphi@163.com>
2024-10-27 23:19:28 +0800

    typo

--- data/siteMetaData.ts
+++ data/siteMetaData.ts
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 
 const siteMetaData: Metadata = {
   title: 'Zottery',
-  description: 'A Excellent Lottery App In Ethereum',
+  description: 'An Excellent Lottery App In Ethereum',
   authors: [{ name: 'Alan Golphi', url: 'https://github.com/AlanGolphi/zottery' }],
   keywords: ['Zottery', 'zottery', 'lottery', 'ethereum', 'web3'],
   referrer: 'strict-origin-when-cross-origin',
